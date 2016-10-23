d5428c545c607443e2a3a9242cb785b9603434e5
Stanimir Stoev <stoevs@gmail.com>
2016-10-23 23:58:13 +0300

    Problems 1 to 16 done

--- IntroToSQL/Homework/IntroToSQLHomework.sql
+++ IntroToSQL/Homework/IntroToSQLHomework.sql
@@ -16,4 +16,21 @@ SELECT FirstName + ' ' + LastName AS 'Full Name' FROM Employees
 
 SELECT FirstName + '.' + LastName + '@telerik.com' AS 'Full Email Address' FROM Employees
 
--- 9. 
+-- 9. Write a SQL query to find all different employee salaries.
+SELECT DISTINCT Salary FROM Employees
+
+-- 10.Write a SQL query to find all information about 
+-- the employees whose job title is “Sales Representative“.
+SELECT * FROM Employees WHERE JobTitle = 'Sales Representative'
+
+-- 11. Write a SQL query to find the names of all employees whose first name starts with "SA"
+SELECT FirstName + ' ' + LastName AS 'Name' FROM Employees WHERE FirstName LIKE 'SA%'
+
+-- 12. Write a SQL query to find the names of all employees whose last name contains "ei".
+SELECT FirstName + ' ' + LastName AS 'Name' FROM Employees WHERE LastName LIKE '%ei%'
+
+-- 13. Write a SQL query to find the salary of all employees whose salary is in the range [20000…30000].
+SELECT FirstName, LastName, Salary FROM Employees WHERE Salary BETWEEN 20000 AND 30000
+
+-- 14. Write a SQL query to find the names of all employees whose salary is 25000, 14000, 12500 or 23600.
+SELECT FirstName, LastName, Salary FROM Employees WHERE Salary IN (25000, 14000, 12500, 23600)
